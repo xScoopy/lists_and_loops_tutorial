44286c2aedcc3d9bc835889a9b8d46fc60aef617
xScoopy <JLeary070@gmail.com>
2020-09-23 15:41:18 -0700

    Finished Questions 1 and 2

---
+++ list_loops.py
@@ -0,0 +1,10 @@
+#Task 1 under Question 1
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+print(songs[1])
+#Task 1 under Question 2
+print(songs[0], songs[2])
+#or
+print(songs[0])
+print(songs[2])
+#Task 2 under Question 2
+print(songs[1:3])
